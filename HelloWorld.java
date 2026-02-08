fc38ab80b3d4fadc933f4646a898de572888cc62
Mai Quoc Toan <quctonnn@gmail.com>
2026-02-08 20:05:34 +0700

    feature/Implement divide

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,7 +13,9 @@ public class HelloWorld {
     }
 
     public static double divide(int a, int b) {
-        throw new UnsupportedOperationException();
+       if (b == 0) throw new ArithmeticException();
+    return (double) a / b;
+
     }
 
     public static void main(String[] args) {
