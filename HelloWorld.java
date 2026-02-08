c575d9a802be3bbe92247dc9c60f41e85ea29d28
Mai Quoc Toan <quctonnn@gmail.com>
2026-02-08 19:47:00 +0700

    Init calculator structure

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+
+    public static int plus(int a, int b) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int minus(int a, int b) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int multiply(int a, int b) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static double divide(int a, int b) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Calculator");
+    }
+}
